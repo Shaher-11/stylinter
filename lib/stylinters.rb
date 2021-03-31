e5b9ae2e8b3cbf92f39e0cd7c17779eee64a7062
Shaher-11 <shahershamroukh@gmail.com>
2021-03-31 16:18:39 +0200

    Add simicolons method

--- lib/stylinters.rb
+++ lib/stylinters.rb
@@ -1,3 +1,5 @@
 module Stylinters
-  
+  def semicolons(line, number)
+    @errors << "Extra semicolon detected on the line number #{number} \u{1f91a} \u{1f91a} \u{1f91a}".colorize(:light_red) if line.include?(';;')
+  end
 end
