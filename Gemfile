f45dfa9f5758b4bdc10fc9fb9d00a30b4ca2843d
Shaher-11 <shahershamroukh@gmail.com>
2021-03-31 15:36:01 +0200

    Add a method to read file and add colorize gem

--- Gemfile
+++ Gemfile
@@ -4,3 +4,4 @@ git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
 
 gem 'rspec'
 gem 'rubocop'
+gem 'colorize'
--- lib/linter.rb
+++ lib/linter.rb
@@ -1,8 +1,18 @@
+require 'colorize'
 class Stylint
   attr_reader :file_path, :errors
 
-  def initialize(file_path, errors)
+  def initialize(file_path)
     @file_path = file_path
-    @errors = errors
+    @errors = []
   end
+
+def check
+  number = 0
+  File.readlines(@file_path).each do |line|
+  number += 1
+  end
+
+  
+
 end
