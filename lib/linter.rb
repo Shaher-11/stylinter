d1a61f601c655c589fc8caaf6dff323888b59e32
Shaher-11 <shahershamroukh@gmail.com>
2021-03-31 15:58:26 +0200

    Create module and chech method

--- lib/linter.rb
+++ lib/linter.rb
@@ -1,5 +1,8 @@
 require 'colorize'
+require_relative('../lib/stylinters')
 class Stylint
+  include Stylinters
+
   attr_reader :file_path, :errors
 
   def initialize(file_path)
@@ -7,12 +10,19 @@ class Stylint
     @errors = []
   end
 
-def check
-  number = 0
-  File.readlines(@file_path).each do |line|
-  number += 1
+  def check
+    number = 0
+    File.readlines(@file_path).each do |line|
+      semicolons(line, number)
+      number += 1
+    end
+
+    if @errors.length.zero?
+      @errors << "All checks have succefully passed ! No errors detected. \u{1F389} \u{1F389} \u{1F389} ".colorize(:light_green)
+    else
+      @errors.sort { |el1, el2| el2 <=> el1 }
+    end
   end
 
   
-
 end
---
+++ lib/stylinters.rb
@@ -0,0 +1,3 @@
+module Stylinters
+  
+end
